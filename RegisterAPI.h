17309b8b080cbec6089e93bd2680015e40cf7a1b
yang99 <996212017@qq.com>
2015-10-20 14:31:26 +0800

    first commit

---
+++ RegisterAPI.h
@@ -0,0 +1,13 @@
+//
+//  RegisterAPI.h
+//  YTKNetworkTest
+//
+//  Created by yangyao on 15/10/20.
+//  Copyright © 2015年 yangyao. All rights reserved.
+//
+
+#import "YTKRequest.h"
+
+@interface RegisterAPI : YTKRequest
+
+@end
---
+++ RegisterAPI.m
@@ -0,0 +1,13 @@
+//
+//  RegisterAPI.m
+//  YTKNetworkTest
+//
+//  Created by yangyao on 15/10/20.
+//  Copyright © 2015年 yangyao. All rights reserved.
+//
+
+#import "RegisterAPI.h"
+
+@implementation RegisterAPI
+
+@end
